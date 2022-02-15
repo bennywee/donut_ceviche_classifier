7e1096cd2ad17408af428502f1a3168503f8259f
occamsblazer3 <benw.cy93@gmail.com>
2022-02-15 20:33:27 +1100

    added frequency table and resize_greyscale tests

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -5,6 +5,7 @@ sys.path.append('/classifier')
 import skimage.io as io
 from skimage import data_dir
 import src.preprocess as pp
+import pandas as pd
 
 
 def test_ioImageCollectio():
@@ -18,6 +19,21 @@ def test_ioImageCollectio():
 
 def test_pixel_dimensions():
     test_data = io.ImageCollection(data_dir + '/r*.jpg')
-    cols = pp.pixel_dimensions(test_data, dim = 0)
+    cols = pp.pixel_dimensions(test_data, dimension = 'width')
     
     assert cols == [1411, 427]
+    
+def test_frequency_table():
+    widths = [500,500,500,400,400,400]
+    heights = [500,500,500,300,300,100]
+    
+    table = pp.frequency_table(width_list = widths,
+                               height_list = heights)
+    
+    assert type(table) == pd.DataFrame
+
+def test_resize_greyscale():
+    img='/classifier/data/ceviche/904.jpg'
+    preprocessed = pp.resize_greyscale(path = img)
+    
+    assert preprocessed['data_pp'][0].shape[0] == 8464
